d01e06821d847e801f48472b2357f330d9f3485e
Katya Novikova <katy.novikova@gmail.com>
2021-03-25 23:50:05 +0200

    feat: make block about project

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './Main.css';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function Main() {
     return (
+        <>
         <Promo />
+        <AboutProject />
+        </>
     )
 }
 
