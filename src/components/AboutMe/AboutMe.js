4b156e5545eb657715a14299721c7e5ec2ac609f
Katya Novikova <katy.novikova@gmail.com>
2021-03-31 18:28:51 +0300

    feat: add Footer

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -14,8 +14,8 @@ function AboutMe() {
                     и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур».
 После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
                     <div className="about__links">
-                    <a target="_blank" href="https://www.facebook.com/" className="about__link" rel="noreferrer">Facebook</a>
-                    <a target="_blank" href="https://github.com/" className="about__link" rel="noreferrer">Github</a>
+                        <a target="_blank" href="https://www.facebook.com/" className="about__link" rel="noreferrer">Facebook</a>
+                        <a target="_blank" href="https://github.com/" className="about__link" rel="noreferrer">Github</a>
                     </div>
                 </div>
                 <img className="about__photo" src={photo} alt="Фото студента" />
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,23 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <div></div>
+        <section className="footer">
+            <div className="footer__block">
+                <p className="footer__text">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+                <div className="footer__info">
+                    <p>&copy; 2020</p>
+                    <ul class="footer__links">
+                        <li>
+                            <a target="_blank" href="https://praktikum.yandex.ru/" rel="noreferrer" class="footer__link">Яндекс.Практикум</a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://github.com/" rel="noreferrer" class="footer__link">Github</a></li>
+                        <li>
+                            <a target="_blank" href="https://www.facebook.com/" rel="noreferrer" class="footer__link">Facebook</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     )
 }
 
