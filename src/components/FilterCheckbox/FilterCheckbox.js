734bc69e6db8c4f58666db8ccf7458707de61bf1
Katya Novikova <katy.novikova@gmail.com>
2021-04-03 22:09:44 +0300

    feat: adaptive movie page

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -5,14 +5,17 @@ import Switch from './Switch';
 function FilterCheckbox(props) {
     return (
         <section className="checkbox">
+            <div className="checkbox__data">
                 <Switch
                     handleColor="white"
                     offColor="white"
                     onColor="#2be080"
-                    // checked={props.checked}
-                    // onChange={checked => { }}
+                    className="checkbox__button"
+                // checked={props.checked}
+                // onChange={checked => { }}
                 />
-            <p className="checkbox__name">Короткометражки</p>
+                <p className="checkbox__name">Короткометражки</p>
+            </div>
         </section>
     )
 }
