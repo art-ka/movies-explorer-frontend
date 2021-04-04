49634f45e893f0ae1bb9faa6a7459d57320c37a5
Katya Novikova <katy.novikova@gmail.com>
2021-04-04 19:11:58 +0300

    feat: add login form

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -5,31 +5,31 @@ import logo from '../../images/logo.svg';
 
 function Register() {
     return (
-        <section className="register">
+        <section className="form">
             <Link to='/'>
-                <img className="register__logo" src={logo} alt="Логотип" />
+                <img className="form__logo" src={logo} alt="Логотип" />
             </Link>
-            <p className="register__welcome">
+            <p className="form__welcome">
                 Добро пожаловать!
         </p>
-            <form className="register__form">
-                <p className="register__name">Имя</p>
-                <input className="register__field register__field_input_name" id="name-input" name="name" type="text"
+            <form className="form__form">
+                <p className="form__name">Имя</p>
+                <input className="form__field form__field_input_name" id="name" name="name" type="text"
                     required minLength="2"/>
-                    <span id="name-input-error" class="register__field-error"></span>
-                <p className="register__name">E-mail</p>
-                <input className="register__field register__field_input_email" id="email" name="email" type="email"
+                    <span id="name-error" class="form__field-error"></span>
+                <p className="form__name">E-mail</p>
+                <input className="form__field form__field_input_email" id="email" name="email" type="email"
                     required />
-                    <span id="name-input-error" class="register__field-error"></span>
-                <p className="register__name">Пароль</p>
-                <input className="register__field register__field_input_password" id="password" name="password" type="password"
+                    <span id="email-error" class="form__field-error"></span>
+                <p className="form__name">Пароль</p>
+                <input className="form__field form__field_input_password" id="password" name="password" type="password"
                     required />
-                    <span id="name-input-error" class="register__field-error">Что-то пошло не так...</span>
-                <button type="submit" className="register__button-container">Зарегистрироваться</button>
+                    <span id="password-error" class="form__field-error">Что-то пошло не так...</span>
+                <button type="submit" className="form__button-container">Зарегистрироваться</button>
             </form>
-            <div className="register__signin">
+            <div className="form__signin">
                 <p>Уже зарегистрированы?
-                <Link to="/signin" className="register__login-link"> Войти</Link> </p>
+                <Link to="/signin" className="form__login-link"> Войти</Link> </p>
             </div>
         </section>
     )
