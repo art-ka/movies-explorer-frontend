d6c637c286085501849bfead77b1d722447e9e74
Katya Novikova <katy.novikova@gmail.com>
2021-04-19 23:55:34 +0300

    fix

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -104,7 +104,7 @@ class Api {
 }
 
 const api = new Api({
-    baseUrl: 'https://api.artmovie.students.nomoredomains.icu/',
+    baseUrl: 'https://api.artmovie.students.nomoredomains.icu',
     headers: {
         authorization: `Bearer ${localStorage.getItem('jwt')}`,
         'Content-Type': 'application/json'
